c3e3d667cb604cd6aec7d0f4218783b664ea01bd
pedro <pedro092692@gmail.com>
2025-01-30 17:25:58 +0100

    create userPost variable

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+var userPost = [];
+
 //define middlewares 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 //app routes 
 app.get('/', (req, res)=>{
-    res.render('index.ejs');
+    res.render('index.ejs', {userPost:userPost});
 });
 
 
