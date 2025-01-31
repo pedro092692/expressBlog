928f240649bada7afba7e6e427a3289b7d02a7bc
pedro <pedro092692@gmail.com>
2025-01-31 14:25:27 +0100

    create redirect function

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ var userPost = [];
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+
+
 //app routes 
 app.get('/', (req, res)=>{
     res.render('index.ejs', {userPost:userPost});
 });
+
+
 
 app.get('/contact', (req, res)=>{
     res.render('contact.ejs');
@@ -35,12 +39,35 @@ app.post('/submit', (req, res)=>{
 
 app.get('/post/:id', (req, res)=>{
     var postId = req.params.id;
+    notFoundRedirect(res, postId);
     var post = userPost[postId]
-    if(!post){
-       res.redirect('/')
-    }
     res.render('post.ejs', post );
 });
+
+app.get('/post/update/:id', (req, res)=>{
+    var postId = req.params.id;
+    notFoundRedirect(res, postId);
+    var post = userPost[postId]
+    post.id = postId;
+    res.render('new-post.ejs', post);
+});
+
+app.post('/update/:id', (req, res)=>{
+    var post =  userPost[req.params.id];
+    post.postContent = req.body.postContent;
+    post.imgURL = req.body.imgURL;
+    post.postName = req.body.postName;
+    res.redirect('/');
+});
+
+//redirect notfound
+function notFoundRedirect(res, id){
+    console.log('hi pedro' + id);
+    var post = userPost[id]
+    if(!post){
+       res.redirect('/');
+    }
+}
 
 
 //start sever
